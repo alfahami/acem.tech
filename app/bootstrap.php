09334c1cfe2c8aba7c7bbcab26edb362a43c3af4
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2022-01-27 22:35:22 +0100

    autoload classes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,16 +2,35 @@
 require_once 'config/config.php';
 //Load helper
 //require_once    'helpers/url_helper.php';
-require_once    'helpers/session_helper.php';
-require_once    'helpers/general_helper.php';
-require_once    'helpers/upload_image.php';
+// require_once    'helpers/session_helper.php';
+// require_once    'helpers/general_helper.php';
+// require_once    'helpers/upload_image.php';
 
-require_once    'librairies/Core.php';
-require_once    'librairies/Controller.php';
-require_once    'librairies/Database.php';
+// require_once    'librairies/Core.php';
+// require_once    'librairies/Controller.php';
+// require_once    'librairies/Database.php';
 
-// Load PHPMailer 
-require 'librairies/PHPMailer/src/PHPMailer.php';
-require 'librairies/PHPMailer/src/SMTP.php';
-require 'librairies/PHPMailer/src/Exception.php';
+// // Load PHPMailer 
+// require 'librairies/PHPMailer/src/PHPMailer.php';
+// require 'librairies/PHPMailer/src/SMTP.php';
+// require 'librairies/PHPMailer/src/Exception.php';
+
+spl_autoload_register();
+
+class AutoLoader {
+    public static function helperLoader($className) {
+
+        require 'helpers/' . $className . '.php';
+    }
+
+    public static function librairieLoader($className) {
+
+        require 'librairies/' . $className . '.php';
+    }
+
+    public static function mailerLoader($sclassName) {
+
+        require 'librairies/PHPMailer/src/' . $className . '.php';
+    }
+}
 
